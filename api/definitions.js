dbebeb2ee32ef6a548491990e6fdfe78bd69e7b3
shawnbirring <shawnbirring21@gmail.com>
2024-02-14 12:11:51 -0800

    get request for set up

--- api/definitions.js
+++ api/definitions.js
@@ -1,12 +1,27 @@
 const http = require("http");
 const url = require("url");
 
+let wordDefinitions = [
+  {
+    word: "word",
+    definition: "a word or something.",
+  },
+];
+
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
-
   if (req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "get" }));
+    const word = parsedUrl.query.word;
+    if (word) {
+      const definition = wordDefinitions[word];
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ word, definition }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({ message: `No definition found for word: ${word}` })
+      );
+    }
   } else if (req.method === "POST") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "post" }));
