1ac0a858001ce1c6bc1351d1b943d5b0c0b7466f
shawnbirring <shawnbirring21@gmail.com>
2024-02-14 11:57:09 -0800

    checking post or get

--- api/definitions.js
+++ api/definitions.js
@@ -2,15 +2,15 @@ const http = require("http");
 const url = require("url");
 
 const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    
-    if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'get' }));
-    } else if (req.method === 'POST') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(parsedUrl));
-    }
+  const parsedUrl = url.parse(req.url, true);
+
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "get" }));
+  } else if (req.method === "POST") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "post" }));
+  }
 });
 
 module.exports = server;
