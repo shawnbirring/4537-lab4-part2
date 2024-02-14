22a038d5b6235595fb8081f6274996726eb28e4f
shawnbirring <shawnbirring21@gmail.com>
2024-02-14 11:51:53 -0800

    initial server

---
+++ api/definitions.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    
+    if (req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'get' }));
+    } else if (req.method === 'POST') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(parsedUrl));
+    }
+});
+
+module.exports = server;
