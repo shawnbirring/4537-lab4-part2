fa42077043abeb6b34635a90973f5cccaff821c0
shawnbirring <shawnbirring21@gmail.com>
2024-02-14 12:19:18 -0800

    moved lower to avoid undefined

--- api/definitions.js
+++ api/definitions.js
@@ -9,8 +9,8 @@ const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   if (req.method === "GET") {
     const word = parsedUrl.query.word;
-    word = word.toLowerCase();
     if (word) {
+      word = word.toLowerCase();
       const definition = wordDefinitions[word];
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ word, definition }));
