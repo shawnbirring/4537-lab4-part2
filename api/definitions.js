ccb2e52564284b7d0c02a67dda45838cb4f333eb
shawnbirring <shawnbirring21@gmail.com>
2024-02-14 12:49:36 -0800

    added in try catch to prevent server crashing on invalid json

--- api/definitions.js
+++ api/definitions.js
@@ -29,14 +29,21 @@ const server = http.createServer((req, res) => {
       body += chunk.toString();
     });
     req.on("end", () => {
-      const { word, definition } = JSON.parse(body);
-      if (wordDefinitions[word]) {
+      try {
+        const { word, definition } = JSON.parse(body);
+        if (wordDefinitions[word]) {
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(
+            JSON.stringify({ message: "word already in", totalRequests })
+          );
+        } else {
+          wordDefinitions[word] = definition;
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ word, definition, totalRequests }));
+        }
+      } catch (error) {
         res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ message: `Word already exists` }));
-      } else {
-        wordDefinitions[word] = definition;
-        res.writeHead(201, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ word, definition, totalRequests }));
+        res.end(JSON.stringify({ message: "cannot parse json" }));
       }
     });
   }
